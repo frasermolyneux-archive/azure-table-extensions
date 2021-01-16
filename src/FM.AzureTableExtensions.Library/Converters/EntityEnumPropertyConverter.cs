1bc887bd25e30ce4cdecd6b5c0ead5fa3ff5fb05
Fraser Molyneux <websites@mx-mail.io>
2021-01-16 23:08:14 +0000

    Adding code copied from XI.Portal repository

---
+++ src/FM.AzureTableExtensions.Library/Converters/EntityEnumPropertyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FM.AzureTableExtensions.Library.Attributes;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace FM.AzureTableExtensions.Library.Converters
+{
+    public class EntityEnumPropertyConverter
+    {
+        public static void Serialize<TEntity>(TEntity entity, IDictionary<string, EntityProperty> results)
+        {
+            entity.GetType().GetProperties()
+                .Where(x => x.GetCustomAttributes(typeof(EntityEnumPropertyConverterAttribute), false).Any())
+                .ToList()
+                .ForEach(x => results.Add(x.Name, new EntityProperty(x.GetValue(entity) != null ? x.GetValue(entity).ToString() : null)));
+        }
+
+        public static void Deserialize<TEntity>(TEntity entity, IDictionary<string, EntityProperty> properties)
+        {
+            entity.GetType().GetProperties()
+                .Where(x => x.GetCustomAttributes(typeof(EntityEnumPropertyConverterAttribute), false).Any())
+                .ToList()
+                .ForEach(x => x.SetValue(entity, Enum.Parse(x.PropertyType, properties[x.Name].StringValue)));
+        }
+    }
+}
---
+++ src/FM.AzureTableExtensions.Library/TableEntityExtended.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FM.AzureTableExtensions.Library.Converters;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace FM.AzureTableExtensions.Library
+{
+    public class TableEntityExtended : TableEntity
+    {
+        public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            var results = base.WriteEntity(operationContext);
+            EntityJsonPropertyConverter.Serialize(this, results);
+            EntityEnumPropertyConverter.Serialize(this, results);
+            return results;
+        }
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties,
+            OperationContext operationContext)
+        {
+            base.ReadEntity(properties, operationContext);
+            EntityJsonPropertyConverter.Deserialize(this, properties);
+            EntityEnumPropertyConverter.Deserialize(this, properties);
+        }
+    }
+}
