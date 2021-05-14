23b599383c1f96f167c94f64c4588fab04f351c5
Fraser Molyneux <websites@mx-mail.io>
2021-05-14 08:54:07 +0100

    Reworking the logging builder extension

--- src/FM.AzureTableLogger/Extensions/ILoggingBuilderExtensions.cs
+++ src/FM.AzureTableLogger/Extensions/ILoggingBuilderExtensions.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using FM.AzureTableLogger.Config;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace FM.AzureTableLogger.Extensions
 {
     // ReSharper disable once InconsistentNaming
     public static class ILoggingBuilderExtensions
     {
-        public static ILoggingBuilder AddAzureTableLogger(this ILoggingBuilder loggingBuilder,
-            IOptions<AzureTableLoggerOptions> configureOptions)
+        public static ILoggingBuilder AddAzureTableLogger(this ILoggingBuilder loggingBuilder)
         {
-            if (configureOptions == null) throw new ArgumentNullException(nameof(configureOptions));
-
-            loggingBuilder.Services.AddLogging(builder =>
-                builder.AddProvider(new AzureTableLoggerProvider(configureOptions)));
-
+            loggingBuilder.Services.AddSingleton<ILoggerProvider, AzureTableLoggerProvider>();
             return loggingBuilder;
         }
     }
