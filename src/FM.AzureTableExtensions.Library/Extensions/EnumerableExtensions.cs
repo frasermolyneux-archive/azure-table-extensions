e7bc85e061a262529d42a9718650737f0c733a4f
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-07-04 10:55:23 +0100

    Adding batch enumerable extension

---
+++ src/FM.AzureTableExtensions.Library/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace FM.AzureTableExtensions.Library.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> collection, int batchSize)
+        {
+            var nextBatch = new List<T>(batchSize);
+            foreach (var item in collection)
+            {
+                nextBatch.Add(item);
+                if (nextBatch.Count == batchSize)
+                {
+                    yield return nextBatch;
+                    nextBatch = new List<T>(batchSize);
+                }
+            }
+
+            if (nextBatch.Count > 0) yield return nextBatch;
+        }
+    }
+}
